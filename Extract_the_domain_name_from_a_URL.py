7dcf753e2b1b54714ed2261b630714545a02406b
vladimir <grun_gespenst@tut.by>
2021-04-18 08:32:08 +0300

    url

---
+++ Extract_the_domain_name_from_a_URL.py
@@ -0,0 +1,26 @@
+def domain_name(url):
+    '''
+    Write a function that when given a URL as a string,
+    parses out just the domain name and returns it as a string. For example:
+    domain_name("http://github.com/carbonfive/raygun") == "github"
+    domain_name("http://www.zombie-bites.com") == "zombie-bites"
+    domain_name("https://www.cnet.com") == "cnet"
+
+    '''
+    if 'www.' in url:
+        start = url.find('www.') + 4
+        step_1 = url[start:]
+    elif '/' in url:
+        start = url.find('//') + 2
+        step_1 = url[start:]
+    else:
+        step_1 = url
+
+    if '.' in url:
+        finish = step_1.find('.')
+        return step_1[:finish]
+    else:
+        return '8' + step_1
+
+
+print(domain_name("29nyl78gz5bzz2d3"))
