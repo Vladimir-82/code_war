800fa3f3bad1446f4c1e75c8e392c6c6183f2289
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-04-21 16:49:10 +0300

    progress

--- Strip_Comments.py
+++ Strip_Comments.py
@@ -19,36 +19,15 @@ def solution(string, markers):
     # result should == "apples, pears\ngrapes\nbananas"
     '''
     splt = string.split('\n')
-    print(splt)
     for el in range(len(splt)):
         for symb in splt[el]:
             if symb in markers:
                 index = splt[el].find(symb)
                 splt[el] = splt[el][:index]
-    print(splt)
     splt = [str(i).strip() for i in splt]
-    print(splt)
     joy = '\\n'.join(splt)
     return joy
 
 
 print(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
 
-# splt = list(string)
-#     for el in range(len(splt)):
-#         if splt[el] == '\n':
-#             splt[el] = '\\n'
-#     string = ''.join(splt)
-#     output = ''
-#     for elem in string:
-#         if elem in markers:
-#             index = string.find(elem)
-#             output += string[:index].strip()
-#             string = string[index + 1:]
-#             if '\\n' in string:
-#                 index_tab = string.find('\\n')
-#                 string = string[index_tab:]
-#             else:
-#                 return output
-#
-# print(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
