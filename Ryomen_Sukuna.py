646a1a778987c55fff3a2f9ad0a9cdf46a028c06
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-04-27 16:53:44 +0300

    how

--- Ryomen_Sukuna.py
+++ Ryomen_Sukuna.py
@@ -4,37 +4,30 @@ def sukuna(n):
     example: if n = 10 then output will be 7, as 4 is representable as 2^2,
     8 as 2^3 and 9 as 3^2. So the numbers left are 1,2,3,5,6,7,10.
     '''
-
-    numbers = [i for i in range(1, n + 1)]
-    out = []
     i, j = 2, 2
-    while True:
-        mult = i ** j
-        if mult in numbers:
-            out.append(mult)
-        j += 1
-        if j > int(n**0.5):
-            j = 2
-            i += 1
-            if i > int(n**0.5):
-                break
-
-    return len(set(numbers) - set(out))
+    out = set()
+    while i ** j <= n:
+        while i ** j <= n:
+            mult = i ** j
+            if mult <= n:
+                out.add(mult)
+            j += 1
+        i += 1
+        j = 2
+    return n - len(out)
 
 start = time.time()
-print(sukuna(20000))
+print(sukuna(1000000))
 finish = time.time()
 print(finish - start)
 
-
-
-# numbers = [i for i in range(1, n + 1)]
-#     out = []
+# numbers = {i for i in range(1, n + 1)}
+#     out = set([])
 #     i, j = 2, 2
 #     while True:
 #         mult = i ** j
-#         if mult in numbers and mult not in out:
-#             out.append(mult)
+#         if mult in numbers:
+#             out.add(mult)
 #         j += 1
 #         if j > int(n**0.5):
 #             j = 2
@@ -42,7 +35,7 @@ print(finish - start)
 #             if i > int(n**0.5):
 #                 break
 #
-#     return n - len(out)
+#     return len(numbers) - len(out)
 #
 # start = time.time()
 # print(sukuna(1000000))
--- Sum_of_Pairs.py
+++ Sum_of_Pairs.py
@@ -17,21 +17,30 @@ def sum_pairs(ints, s):
     #  there are no pairs of values that can be added to produce 2.
     == None/nil/undefined (Based on the language)
     '''
-    i = 1
+    control = len(ints) - 1
     k = 0
+    i = 1
+    summ = []
     while True:
         res = ints[k] + ints[i]
-        if res == s:
-            return [ints[k], ints[i]]
+        if res == s and i <= control:
+            summ = [ints[k], ints[i]]
+            ints = ints[:i]
+            if len(ints) <= 2:
+                return summ
+            control = i
+
         i += 1
-        if k == len(ints) - 2:
-            return None
-        res = ints[k] + ints[i]
-        if res == s:
-            return [ints[k], ints[i]]
-        k += 1
-        i = k + 1
-print(sum_pairs([1, 4, 8, 7, 3, 15], 8))
+        if i >= len(ints):
+            k += 1
+            if k == len(ints) - 1:
+                break
+            i = k + 1
+
+    return None if not summ else summ
+print(sum_pairs([1, 1], 2))
+
+
 # indx = len(ints) - 1
 #     summ = []
 #     for k in range(len(ints) - 1):
