6253f6f930d6d01a79e67be2c2babfbb9dd40da8
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-05-06 16:52:58 +0300

    merged

--- Merged_String_Checker.py
+++ Merged_String_Checker.py
@@ -5,22 +5,38 @@ def is_merge(s, part1, part2):
     part1:  c   d   w         = cdw
     part2:    o   e   a r s   = oears
     '''
-    for i in range(len(s)):
-
-        if s[i] == part1[:i] == part2[:i]:
-            continue
-        else:
-            if s[i] != part1[:i]:
-                part2 = part2[:i]
-            else:
-                part1 = part1[:i]
-
-        if part1 and s[i] == part1[0]:
-            part1 = part1[1:]
-        elif part2 and s[i] == part2[0]:
-            part2 = part2[1:]
-        else:
+    stop = 1
+    while s:
+        if s[:stop] != part1[:stop] and s[:stop] != part2[:stop]:
             return False
-    return True if not part1 and not part2 else False
+        if s[:stop] == part1[:stop] == part2[:stop]:
+            stop += 1
+        else:
+            if s[:stop] == part1[:stop]:
+                part1 = part1[stop:]
 
-print((is_merge('Bananas from Bahamas', 'Bahas', 'Bananas from am')))
+            else:
+                part2 = part2[stop:]
+            s = s[stop:]
+            stop = 1
+
+    return True if not part1 and not part2 else False
+print((is_merge('codewars', 'code', 'wars')))
+
+# for i in range(len(s)):
+#
+#     if s[i] == part1[:i] == part2[:i]:
+#         continue
+#     else:
+#         if s[i] != part1[:i]:
+#             part2 = part2[:i]
+#         else:
+#             part1 = part1[:i]
+#
+#     if part1 and s[i] == part1[0]:
+#         part1 = part1[1:]
+#     elif part2 and s[i] == part2[0]:
+#         part2 = part2[1:]
+#     else:
+#         return False
+# return True if not part1 and not part2 else False
