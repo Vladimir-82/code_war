e0db41d74c9fcd5e586460f0317d993b4e8e553d
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-06-04 15:48:21 +0300

    camelCase

--- Simple_Fun118_Sum_Or_Product.py
+++ Simple_Fun118_Sum_Or_Product.py
@@ -17,24 +17,39 @@ def sum_or_product(arr):
     ([1, 1, 2, 4, 5],  80),
     ([10, 20, 30],  6000)
     '''
-    while True:
-        if len(arr) == 1:
-            return arr[0]
 
-        if arr[0] == 1 or arr[1] == 1:
-            if 1 <= arr[0] <= 2:
-                s = arr[0] + arr[1]
-                arr[0:2:] = [s]
-
-        else:
-            s = arr[0] * arr[1]
+    if len(arr) == 1:
+        return arr[0]
+    while 1 in arr:
+        if 1 <= arr[0] <= 2:
+            s = arr[0] + arr[1]
             arr[0:2:] = [s]
-
-
-        if arr[0] >= 3:
             arr.append(arr[0])
             del arr[0]
 
+    n = 1
+    for i in arr:
+        n *= i
+    return n
 
 
-print(sum_or_product([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+
+
+print(sum_or_product([2, 2, 2, 2, 2, 2, 2, 2, 2, 2]))
+
+# while True:
+#     if len(arr) == 1:
+#         return arr[0]
+#
+#     if arr[0] == 1 or arr[1] == 1:
+#         if 1 <= arr[0] <= 2:
+#             s = arr[0] + arr[1]
+#             arr[0:2:] = [s]
+#
+#     else:
+#         s = arr[0] * arr[1]
+#         arr[0:2:] = [s]
+#
+#     if arr[0] >= 3:
+#         arr.append(arr[0])
+#         del arr[0]
