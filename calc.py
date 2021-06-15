0d3d1fabbad1e0c61baadf2fd18945dcffe78e57
vladimir <grun_gespenst@tut.by>
2021-06-15 19:15:05 +0300

    chimistry

--- calc.py
+++ calc.py
@@ -1,19 +1,41 @@
 def parse_molecule(formula):
     for el in formula:
-        if el == '(':
-            insert = ''
-            start = formula.find(el)
-            finish = formula.find(')')
+        if el.isdigit():
+            index = formula.find(el)
+            if formula[index-1].isalpha():
+                insert = int(formula[index]) * formula[index-1]
+                formula = formula[:index-1] + insert + formula[index+1:]
 
-            for i in formula[start + 1:finish]:
-                if i.isdigit():
-
-                insert += int(formula[finish + 1]) * i
-
-            print(insert)
-            formula = formula[:start] + insert + formula[finish + 1:]
     return formula
 
 
 print(parse_molecule('K4[ON(SO3)2]2'))
 # return {K: 4, O: 14, N: 2, S: 4}
+
+
+
+
+
+
+
+
+
+# def parse_molecule(formula):
+#     for el in formula:
+#         if el == '(':
+#             insert = ''
+#             start = formula.find(el)
+#             finish = formula.find(')')
+#
+#             for i in formula[start + 1:finish]:
+#                 if i.isdigit():
+#
+#                 insert += int(formula[finish + 1]) * i
+#
+#             print(insert)
+#             formula = formula[:start] + insert + formula[finish + 1:]
+#     return formula
+#
+#
+# print(parse_molecule('K4[ON(SO3)2]2'))
+#  return {K: 4, O: 14, N: 2, S: 4}
