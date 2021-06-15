16839e9a3ef6e028fb17b1cbff10e19192457223
vladimir <grun_gespenst@tut.by>
2021-06-15 20:34:52 +0300

    chem

--- calc.py
+++ calc.py
@@ -5,37 +5,29 @@ def parse_molecule(formula):
             if formula[index-1].isalpha():
                 insert = int(formula[index]) * formula[index-1]
                 formula = formula[:index-1] + insert + formula[index+1:]
+    insert = ''
+    for el in formula:
+        if el == '(':
+            start = formula.find(el)
+            finish = formula.find(')')
 
-    return formula
+            for i in formula[start + 1:finish]:
+                insert += int(formula[finish + 1]) * i
+            formula = formula[:start] + insert + formula[finish + 2:]
+
+    insert = ''
+    for el in formula:
+        if el == '[':
+            start = formula.find(el)
+            finish = formula.find(']')
+
+            for i in formula[start + 1:finish]:
+                insert += int(formula[finish + 1]) * i
+
+            formula = formula[:start] + insert + formula[finish + 2:]
+
+    dc = {i:formula.count(i) for i in formula if i.isupper()}
+    return dc
 
 
-print(parse_molecule('K4[ON(SO3)2]2'))
-# return {K: 4, O: 14, N: 2, S: 4}
-
-
-
-
-
-
-
-
-
-# def parse_molecule(formula):
-#     for el in formula:
-#         if el == '(':
-#             insert = ''
-#             start = formula.find(el)
-#             finish = formula.find(')')
-#
-#             for i in formula[start + 1:finish]:
-#                 if i.isdigit():
-#
-#                 insert += int(formula[finish + 1]) * i
-#
-#             print(insert)
-#             formula = formula[:start] + insert + formula[finish + 1:]
-#     return formula
-#
-#
-# print(parse_molecule('K4[ON(SO3)2]2'))
-#  return {K: 4, O: 14, N: 2, S: 4}
+print(parse_molecule("Mg(OH)2"))
