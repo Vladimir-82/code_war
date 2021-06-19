498b9687d23b148c3c46a5df686099721ddb4aa3
vladimir <grun_gespenst@tut.by>
2021-06-19 10:57:22 +0300

    chem

--- calc.py
+++ calc.py
@@ -1,33 +1,64 @@
 def parse_molecule(formula):
+    formula = list(formula)
+    print(formula)
+    for i in formula:
+        if i.islower():
+            index = formula.index(i)
+            formula[index - 1] = formula[index - 1] + formula[index]
+            del formula[index]
+    print(formula)
+
     for el in formula:
         if el.isdigit():
-            index = formula.find(el)
+            index = formula.index(el)
             if formula[index-1].isalpha():
-                insert = int(formula[index]) * formula[index-1]
-                formula = formula[:index-1] + insert + formula[index+1:]
-    insert = ''
-    for el in formula:
-        if el == '(':
-            start = formula.find(el)
-            finish = formula.find(')')
-
-            for i in formula[start + 1:finish]:
-                insert += int(formula[finish + 1]) * i
-            formula = formula[:start] + insert + formula[finish + 2:]
-
-    insert = ''
-    for el in formula:
-        if el == '[':
-            start = formula.find(el)
-            finish = formula.find(']')
-
-            for i in formula[start + 1:finish]:
-                insert += int(formula[finish + 1]) * i
-
-            formula = formula[:start] + insert + formula[finish + 2:]
-
-    dc = {i:formula.count(i) for i in formula if i.isupper()}
-    return dc
+                print(formula[index-1])
 
 
-print(parse_molecule("Mg(OH)2"))
+
+    print(formula)
+
+
+    # dc = {i:formula.count(i) for i in formula}
+    # return dc
+
+
+print(parse_molecule('Mg4[ON(SO3)2]2'))
+
+
+
+
+
+# def parse_molecule(formula):
+#     for el in formula:
+#         if el.isdigit():
+#             index = formula.find(el)
+#             if formula[index-1].isalpha():
+#                 insert = int(formula[index]) * formula[index-1]
+#                 formula = formula[:index-1] + insert + formula[index+1:]
+#     insert = ''
+#     for el in formula:
+#         if el == '(':
+#             start = formula.find(el)
+#             finish = formula.find(')')
+#
+#             for i in formula[start + 1:finish]:
+#                 insert += int(formula[finish + 1]) * i
+#             formula = formula[:start] + insert + formula[finish + 2:]
+#
+#     insert = ''
+#     for el in formula:
+#         if el == '[':
+#             start = formula.find(el)
+#             finish = formula.find(']')
+#
+#             for i in formula[start + 1:finish]:
+#                 insert += int(formula[finish + 1]) * i
+#
+#             formula = formula[:start] + insert + formula[finish + 2:]
+#
+#     dc = {i:formula.count(i) for i in formula if i.isupper()}
+#     return dc
+#
+#
+# print(parse_molecule("Mg(OH)2"))
